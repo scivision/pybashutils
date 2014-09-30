718ae80a636d6f3b2842e997f384b27a6977b842
scivision <scivision@users.noreply.github.com>
2014-09-30 11:00:51 -0400

    initial commit

---
+++ pdf2png.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# uses Ghostscript to convert PDF to PNG at good quality
+# Michael Hirsch
+
+inpdf=$1
+firstpg=$2
+lastpg=$3
+dpi=300 #user specified
+
+#basestem=$(basename $inpdf .pdf) #no directory
+instem=${inpdf%.*} #no $ on inpdf
+
+gs -r$dpi -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m \
+	-dFirstPage=$firstpg -dLastPage=$lastpg \
+	-sOutputFile="$instem.png" "$inpdf"
