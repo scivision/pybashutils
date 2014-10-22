7ba043c9201a0ee349f8968549b6517aaebe2f96
scivision <scivision@users.noreply.github.com>
2014-10-22 15:33:04 -0400

    initial commit

---
+++ findtext.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+maxFileSize=10M  #don't search inside huge binary files
+
+case $# in
+1)
+srchText=$1
+fDir="."
+fntmp="*" ;;
+2)
+fDir=$1
+srchText=$2
+fntmp="*" ;;
+3)
+fDir=$1
+srchText=$2
+fntmp=$3 ;;
+*)
+echo usage: "findtext [DIR] TEXT [FILENAME]"
+echo searches for TEXT under DIR and echos back filenames
+exit ;;
+esac
+
+# dont use -execdir, or you wont see which directory the found files are in!
+find $fDir -type f -name "$fntmp" -size -$maxFileSize \
+    -exec grep --ignore-case --files-with-matches "$srchText" {} +
