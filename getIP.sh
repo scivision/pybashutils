5267825e641c1f9aa57a62c05311fc04acddd5ec
scivision <scivision@users.noreply.github.com>
2014-11-11 00:17:35 -0500

    more addresses'

--- getIP.sh
+++ getIP.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #gets public IP address and displays in Terminal
+# Michael Hirsch
 
-IPurl3="wtfismyip.com/text"
-IPurl2="ipecho.net/plain" #this one has become unreliable
-IPurl="v4.ident.me"
+url=('v4.ident.me' 'ipecho.net/plain' 'wtfismyip.com/text')
 
-if curl -m 4 $IPurl; then echo
-elif curl -m 4 $IPurl2; then echo
-else echo Could not connect to $IPurl, sorry
-fi
+for u in ${url[@]}; do
+ if curl -m 4 $u; then echo; break
+ fi
+done
 
 
 
