372e106d57b2cabb8da51e4852cae6101ef93879
scivision <scivision@users.noreply.github.com>
2015-01-29 14:55:47 -0500

    update

--- cupd.sh
+++ cupd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image opencv bokeh mkl python conda ipython ephem
+conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image opencv bokeh mkl python conda ipython ephem pip
