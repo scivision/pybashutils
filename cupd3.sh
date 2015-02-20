c58fdaa672ee575ef6817e0b2b222ea5391f684e
scivision <scivision@users.noreply.github.com>
2015-02-19 22:16:25 -0500

    numba

--- cupd3.sh
+++ cupd3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/anaconda3/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip
+~/anaconda3/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba
