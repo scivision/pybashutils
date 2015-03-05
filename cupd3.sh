d772657905fe466e4bea82a542c0a42dd35f521c
scivision <scivision@users.noreply.github.com>
2015-03-05 18:23:37 -0500

    multipath

--- cupd3.sh
+++ cupd3.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-
-~/anaconda3/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
+#!/bin/bash
+for d in anaconda3 miniconda3
+ do
+ if [[ -d ~/$d ]]; then
+   ~/$d/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
+ fi
+done
