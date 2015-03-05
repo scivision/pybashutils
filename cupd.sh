1930345c006b65f50c549cbbff01ff1a52f9ac22
scivision <scivision@users.noreply.github.com>
2015-03-05 18:33:05 -0500

    automation

--- cupd.sh
+++ cupd.sh
@@ -2,6 +2,6 @@
 for d in anaconda miniconda
  do
  if [[ -d ~/$d ]]; then
-~/$d/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image opencv bokeh mkl python conda ipython ephem pip numba spyder
+~/$d/bin/conda install --yes matplotlib scipy numpy astropy pandas xlrd h5py scikit-image opencv bokeh mkl python conda ipython ephem pip numba spyder
  fi
 done
--- cupd3.sh
+++ cupd3.sh
@@ -2,6 +2,6 @@
 for d in anaconda3 miniconda3
  do
  if [[ -d ~/$d ]]; then
-   ~/$d/bin/conda update matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
+   ~/$d/bin/conda install --yes matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
  fi
 done
