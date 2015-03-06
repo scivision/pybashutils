4fd39b2122546cf49a4cc3c9298c2e5daafd7939
scivision <scivision@users.noreply.github.com>
2015-03-05 21:49:46 -0800

    all in one

--- cupd.sh
+++ cupd.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-for d in anaconda miniconda
+
+#detect operating system
+case "$(uname -s)" in
+    CYGWIN*) hd=$SYSTEMDRIVE; sd=Scripts ;;
+    *)       hd=$HOME; sd=bin ;;
+esac
+
+for d in anaconda miniconda anaconda3 miniconda3
  do
- if [[ -d ~/$d ]]; then
-~/$d/bin/conda install --yes matplotlib scipy numpy astropy pandas xlrd h5py scikit-image opencv bokeh mkl python conda ipython ephem pip numba spyder
+ if [[ -d $hd/$d ]]; then
+  $hd/$d/$sd/conda install --yes matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
+  # now let's try opencv, which isn't always available
+   if $hd/$d/$sd/conda install --yes opencv; then :
+   fi 
+
  fi
 done
--- cupd3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for d in anaconda3 miniconda3
- do
- if [[ -d ~/$d ]]; then
-   ~/$d/bin/conda install --yes matplotlib scipy numpy astropy pandas xlrd h5py scikit-image bokeh mkl python conda ipython ephem pip numba spyder
- fi
-done
