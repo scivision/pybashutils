7de47eda1292cf5bdf2490c0a3a4896031fa9ac2
scivision <scivision@users.noreply.github.com>
2015-03-30 12:44:29 -0400

    random delay for crude rate limiting

--- gtps.sh
+++ gtps.sh
@@ -5,5 +5,5 @@ rdir=~/code/
 for cdr in $(find $rdir -mindepth 1 -maxdepth 1 -type d); do
       echo "pushing $cdr"
       (cd "$cdr" && git commit -a; git push)
-       sleep 0.$[ ($RANDOM % 1000)+0.2 ] #so as not to hammer the remote server$
+       sleep 0.$[ ($RANDOM % 1000) ] #so as not to hammer the remote server
 done
