66cfce271410ebcee0ba24ad5545131eecddd105
scivision <scivision@users.noreply.github.com>
2015-03-30 12:43:41 -0400

    random delay for crude rate limiting

--- gtpl.sh
+++ gtpl.sh
@@ -7,4 +7,5 @@ rdir=~/code/
 for cdr in $(find $rdir -mindepth 1 -maxdepth 1 -type d); do
        echo "pulling $cdr"
        (cd "$cdr" && exec git pull)
+       sleep 0.$[ ($RANDOM % 1000)+0.2 ] #so as not to hammer the remote server, delay of 0.2-1.2 second
 done
