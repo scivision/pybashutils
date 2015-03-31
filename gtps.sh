296db7b53ff70a7caaa37ac7a7e41faf8d697897
scivision <scivision@users.noreply.github.com>
2015-03-31 14:34:32 -0400

    smoother faster operations

--- gtps.sh
+++ gtps.sh
@@ -3,7 +3,7 @@
 rdir=~/code/
 
 for cdr in $(find $rdir -mindepth 1 -maxdepth 1 -type d); do
-      echo "pushing $cdr"
-      (cd "$cdr" && git commit -a; git push)
-       sleep 0.$[ ($RANDOM % 1000) ] #so as not to hammer the remote server
+      
+(cd $cdr; [[ -n `git diff HEAD` ]] 2>/dev/null && (echo "pushing $cdr"; git commit -a; git push; sleep 0.$[ ($RANDOM % 1000) ]))
+
 done
