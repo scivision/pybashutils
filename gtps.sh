184e85194f61b7dec6235defb8daffa3577e3835
scivision <scivision@users.noreply.github.com>
2015-04-03 19:06:11 -0400

    cleaner operation

--- gtps.sh
+++ gtps.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 rdir=~/code/
-
+cdir=$(pwd)
 for cdr in $(find $rdir -mindepth 1 -maxdepth 1 -type d); do
-      
-(cd $cdr; [[ -n `git diff HEAD` ]] 2>/dev/null && (echo "pushing $cdr"; git commit -a; git push; sleep 0.$[ ($RANDOM % 1000) ]))
-
+cd $cdr
+echo $(pwd)
+  if [[ -n `git --no-pager diff HEAD` ]]; then
+    git diff HEAD
+    git commit -a
+    git push
+    sleep 0.$[ ($RANDOM % 1000)]
+  fi
 done
+cd $pwd
