0b7aca3c5061e080feb2d3facd1798406b2ce753
scivision <scivision@users.noreply.github.com>
2015-05-28 16:27:48 -0400

    httpS by default

--- getIP.sh
+++ getIP.sh
@@ -2,6 +2,15 @@
 # gets public IP address and displays in Terminal
 # Michael Hirsch
 # https://scivision.co
+#
+# Note: if you get error message curl: (77) error setting certificate verify locations:
+# curl can't find your certificates. Fix this by:
+#
+# sudo mkdir -p /etc/pki/tls/certs
+# sudo ln /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt
+#
+# Ref: http://stackoverflow.com/questions/3160909/how-do-i-deal-with-certificates-using-curl-while-trying-to-access-an-https-url
+#
 
 url=('https://api.ipify.org' 'http://v4.ident.me' 'http://ipecho.net/plain' 'http://wtfismyip.com/text')
 
