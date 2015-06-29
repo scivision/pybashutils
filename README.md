90e653ef3c4d10e7107d9433407aceb65f97110b
scivision <scivision@users.noreply.github.com>
2015-06-29 11:41:26 -0400

    travis nose

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/scienceopen/pybashutils.svg)](https://travis-ci.org/scienceopen/pybashutils)
+
 bash-utils
 ==========
 
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from numpy.testing import assert_almost_equal
 #%% findnearest
-from findnearest import find_nearest
-indf,xf = find_nearest([10,15,12,20,14,33],[32,12.01])
-assert_almost_equal(indf,[5,2])
-assert_almost_equal(xf,[33.,12.])
+from .findnearest import find_nearest
+
+def test_findnearest():
+    indf,xf = find_nearest([10,15,12,20,14,33],[32,12.01])
+    assert_almost_equal(indf,[5,2])
+    assert_almost_equal(xf,[33.,12.])
 
