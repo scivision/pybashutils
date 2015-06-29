f129d7598ee5e45381298b20f7bdff518cdfd27a
scivision <scivision@users.noreply.github.com>
2015-06-29 14:14:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from numpy.testing import assert_almost_equal
 #%% findnearest
-from .findnearest import find_nearest
+try:
+    from .findnearest import find_nearest
+except:
+    from findnearest import find_nearest
 
 def test_findnearest():
     indf,xf = find_nearest([10,15,12,20,14,33],[32,12.01])
