b59e095f908083296de41e77dd95f7afe573ff66
scivision <scivision@users.noreply.github.com>
2015-11-01 20:30:14 -0500

    no est

--- gtps.sh
+++ gtps.sh
@@ -18,6 +18,11 @@ ldir=$(find -H $rdir -mindepth 1 -maxdepth 1 -type d)
   for cdr in ${ldir[@]}; do
     cd $cdr
      echo -e "\n ---> $cdr"
+
+#    if [[ -s test/test.py ]]; then
+#      nosetests -v test/test.py
+#    fi
+
     if [[ -n $(git --no-pager diff HEAD 2>/dev/null) ]]; then
       git diff HEAD
       git commit -a && git push
