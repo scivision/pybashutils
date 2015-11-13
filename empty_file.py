b3653a2070d1673e35b9032da3b717a7b1eab576
scivision <scivision@users.noreply.github.com>
2015-11-13 13:18:46 -0500

    move

---
+++ empty_file.py
@@ -0,0 +1,35 @@
+"""
+empty_file.py
+makes empty file(s)
+in bash you'd do  >myfile or touch myfile
+"""
+from __future__ import unicode_literals
+from six import string_types,PY2
+from pathlib2 import Path
+if PY2: FileNotFoundError=IOError
+
+def empty_file(flist):
+    if isinstance(flist,(string_types,Path)):
+        flist=[flist]
+
+    for f in flist:
+       _blank(f)
+
+
+def touch(f):
+    f = Path(f).expanduser()
+
+    try:
+        f.write_text('')
+    except FileNotFoundError:
+        f.mkdir(parents=True,exist_ok=True)
+        f.write_text('')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('dirs',help='directories to create',nargs='+',default='')
+    p = p.parse_args()
+
+    empty_file(p.dirs)
