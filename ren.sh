1d9be8266e49869a12b4d4c35d0e04abb13abaf1
scivision <scivision@users.noreply.github.com>
2016-01-14 21:24:11 -0500

    using input pattern

--- ren.sh
+++ ren.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for f in $(ls *.png); do
+pat=$1
+[[ -z $pat ]] && { echo "must specify a file pattern to process"; exit 1; }
+
+for f in $(ls -A $pat); do
 #g=$(echo $f | sed -e 's/\.//') #removes first (leftmost) dot in filename. FFMPEG can handle only one dot in filename.
 g=$(echo $f | sed -e 's/\:/-/g') #removes all colons in filename, which FFMPEG can't handle
 #echo $g
