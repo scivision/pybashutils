3543ba4be47df25a5d5642723d20d50cd23cdf7b
scivision <scivision@users.noreply.github.com>
2016-01-18 02:33:10 -0500

    upd

--- ren.sh
+++ ren.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#don't mess with the quotes on $pat. You will need to "" quote your input pattern
+
 pat=$1
 [[ -z $pat ]] && { echo "must specify a file pattern to process"; exit 1; }
 
 for f in $(ls -A $pat); do
-#g=$(echo $f | sed -e 's/\.//') #removes first (leftmost) dot in filename. FFMPEG can handle only one dot in filename.
-g=$(echo $f | sed -e 's/\:/-/g') #removes all colons in filename, which FFMPEG can't handle
+g=$(echo $f | sed -e 's/\.//') #removes first (leftmost) dot in filename. FFMPEG can handle only one dot in filename.
+#g=$(echo $f | sed -e 's/\:/-/g') #removes all colons in filename, which FFMPEG can't handle
 #echo $g
 mv -iv $f $g
 done
