0486a136cea446634b7d708cca743faa255315d9
scivision <scivision@users.noreply.github.com>
2016-03-29 02:56:58 -0400

    unused

--- cupd.sh
+++ cupd.sh
@@ -19,14 +19,10 @@ for d in anaconda anaconda2 miniconda miniconda2 anaconda3 miniconda3
  do
  if [[ -d $hd/$d ]]; then
   cdir=$hd/$d/$sd
-  $cdir/conda install --yes matplotlib seaborn scipy numpy astropy pandas xlrd h5py scikit-image bokeh python conda ipython ephem pip numba spyder ipython-notebook paramiko basemap jedi pylint netcdf4 requests #mkl
+  $cdir/conda install --yes matplotlib seaborn scipy numpy astropy pandas xlrd h5py scikit-image bokeh python conda ipython ephem pip numba spyder ipython-notebook paramiko basemap jedi pylint netcdf4 requests xarray #mkl
   $cdir/pip install --upgrade plotly
   $cdir/pip install --upgrade tifffile
   $cdir/pip install --upgrade oct2py spectral spacepy
 
-  if [[ "${d: -1}" -ne 3 ]]; then #python 2.7
-    $cdir/pip install --upgrade xray bottleneck cyordereddict
-    $cdir/pip install --upgrade pydap #python 2.7
-  fi
  fi
 done
