89681ab40114c31aa931c742e0fc71729a6b5aa2
scivision <scivision@users.noreply.github.com>
2016-06-21 15:25:53 -0600

    init

--- git_filemode_windows.py
+++ git_filemode_windows.py
@@ -4,25 +4,10 @@ sets core.fileMode=false for git repos
 mostly for Windows, particularly Cygwin
 """
 from pythonutils import Path
+from pythonutils.ChDir import ChDir
 from platform import system
 import os
 from subprocess import call
-
-class ChDir(object):
-    """
-    context manager for changing directories
-    from https://pythonadventures.wordpress.com/2013/12/15/chdir-a-context-manager-for-switching-working-directories/
-    Step into a directory temporarily.
-    """
-    def __init__(self, path):
-        self.old_dir = os.getcwd()
-        self.new_dir = path
-
-    def __enter__(self):
-        os.chdir(str(self.new_dir))
-
-    def __exit__(self, *args):
-        os.chdir(str(self.old_dir))
 
 # autodetect root directory  c:\code or ~/code  arbitrary choice
 plat = system().lower()
---
+++ pythonutils/ChDir.py
@@ -0,0 +1,17 @@
+import os
+
+class ChDir(object):
+    """
+    context manager for changing directories -- compatible with pathlib
+    from https://pythonadventures.wordpress.com/2013/12/15/chdir-a-context-manager-for-switching-working-directories/
+    Step into a directory temporarily.
+    """
+    def __init__(self, path):
+        self.old_dir = os.getcwd()
+        self.new_dir = path
+
+    def __enter__(self):
+        os.chdir(str(self.new_dir))
+
+    def __exit__(self, *args):
+        os.chdir(str(self.old_dir))
