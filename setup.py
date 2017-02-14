11c0af82d5bb59c1b692622d60d1a701e60ca785
scivision <scivision@users.noreply.github.com>
2017-02-14 18:16:07 -0500

    selftest

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 req = ['nose','colorama','binaryornot']
 
 setup(name='pythonutils',
-	  install_requires=req,
       packages=['pythonutils'],
+      author='Michael Hirsch, Ph.D.',
+      description='Cross-platform utilities for computer maintenance',
+	  install_requires=req,
 	  )
