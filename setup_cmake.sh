069561818e3172814c6a82880287626cd3159eb1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-07-05 17:50:52 -0400

    non sudo install

--- setup_cmake.sh
+++ setup_cmake.sh
@@ -3,15 +3,26 @@
 # requires libncurses-dev
 set -e
 
+INSTALL_DIR=$1
+
 (
 cver=3.9.0-rc5
 
-wd=$(mktemp -d)
+wd=/tmp
+
 wget -nc -P $wd https://cmake.org/files/v${cver:0:3}/cmake-$cver.tar.gz
+
 cd $wd
+
 tar -xf cmake-$cver.tar.gz
-./cmake-$cver/configure
+
+./cmake-$cver/bootstrap --prefix=INSTALL_DIR
+
 make -j4
-sudo make install
+
+make install
+
 echo "reopen a new terminal to use CMake $cver"
+
+[[ -z $INSTALL_DIR ]] && echo " you may wish to add $INSTALL_DIR to your PATH"
 )
