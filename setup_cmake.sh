7f66a4136d83e8bd66cdf862aa5aa2d98e22c0a7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-07-25 15:51:35 -0400

    Update setup_cmake.sh

--- setup_cmake.sh
+++ setup_cmake.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 # requires libncurses-dev
 set -e
 
-(
 cver=3.9.0
 
 wd=/tmp
-
 wget -nc -P $wd https://cmake.org/files/v${cver:0:3}/cmake-$cver.tar.gz
 
+(
 cd $wd
 
 tar -xf cmake-$cver.tar.gz
