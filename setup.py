b16a1c709fa8d2e085676becf830fdeb72e36ee8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-27 00:16:09 -0500

    setup.py template

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-req = ['nose','colorama','binaryornot']
+install_requires=['colorama','binaryornot']
+tests_require=['nose','coveralls']
 # %%
 from setuptools import setup,find_packages
 
 setup(name='pythonutils',
       packages=find_packages(),
       author='Michael Hirsch, Ph.D.',
+      version='0.5.0',
       description='Cross-platform utilities for computer maintenance',
       url='https://github.com/scivison/pybashutils',
-      install_requires=req,
+      install_requires=install_requires,
       python_requires='>=3.5',
+      tests_require=tests_require,
+      extras_require={'tests':tests_require},
 	  )
