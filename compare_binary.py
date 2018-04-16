f23965cf6f49bb86b8490c330f5ef95cebcd33c0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-04-16 10:21:37 -0400

    init

---
+++ compare_binary.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+compare binary output files
+"""
+import filecmp
+from pathlib import Path
+
+def compbin(dir1:Path, dir2:Path, pat:str):
+    dir1 = Path(dir1).expanduser()
+    dir2 = Path(dir2).expanduser()
+    
+    fl1 = dir1.glob(pat)
+    fl2 = dir2.glob(pat)
+    
+    for f,g in zip(fl1, fl2):
+        if not filecmp.cmp(f,g, False):
+            print('difference:',f.name)
+          
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('dirs',help='dir1 dir2 to compare files matching pat',nargs=2)
+    p.add_argument('pat',help='filename pattern')
+    p = p.parse_args()
+    
+    compbin(*p.dirs,p.pat)
