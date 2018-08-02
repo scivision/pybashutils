82f0864a9330a909a2f03c8571f3c2f12f28662b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-01 23:44:58 -0400

    test use specific directory in case of varying systems

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import subprocess
 import pytest
+from pathlib import Path
+
+R = Path(__file__).parent
 
 
 def test_findtext():
-    ret = subprocess.check_output(['findtext', 'import'], universal_newlines=True)
+    ret = subprocess.check_output(['findtext', 'import'], universal_newlines=True, cwd=R)
 
     assert isinstance(ret, str)
     assert len(ret) > 0
